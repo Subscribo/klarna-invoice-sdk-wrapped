8f2a3c46f5b5e3f16f74e8352c38f63e7dc174f5
slavo <slavo2@users.noreply.github.com>
2016-06-30 18:05:35 +0200

    Added Klarna class load helper file

---
+++ lib/classloadhelpers/Klarna.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Klarna class load helper requiring Klarna class file
+ * and also part of phpxmlrpc 3->4 compatibility layer
+ *
+ * Note: not part of the original Klarna PHP SDK
+ *
+ */
+
+require_once __DIR__ . "/../../../phpxmlrpc/phpxmlrpc/lib/xmlrpc.inc";
+
+require_once __DIR__ . "../src/Klarna.php";
